9810f0e01feb73c9bb29f5c7e9d48aa15646e9fe
Monika Borkowska <monikademianiuk@gmail.com>
2021-09-19 13:01:56 +0200

    application update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   const apiRoot = 'https://dry-ravine-53349.herokuapp.com/v1/tasks';
+   const apiRoot = 'https://dry-ravine-53349.herokuapp.com/v1/';
    const trelloApiRoot = 'https://dry-ravine-53349.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
